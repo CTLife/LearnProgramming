76eb71a2da659f44d6cc1ee19705e8455c06ab10
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-03-21 23:49:12 +0800

    Create 1.py

---
+++ 5-Python3/1-PythonBaoDian/Chapter-2/1.py
@@ -0,0 +1,85 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/env python3
+
+import os,sys,time,math,re
+
+
+a=3
+b=30
+if a>b:
+    print(a)
+else:
+    print(b)
+
+
+print('仿Mac OS X界面的Linux系统出“新版”了')
+print('\n\n\n###########################################################################\n\n\n')
+
+
+
+if a>b:
+    if a==1:
+        print(a)
+    else:
+        if  a==0:
+            print(a)
+        else:
+            pass
+elif  a==b:
+    print(a,b)
+else:
+    print(b)
+
+
+
+print('\n\n\n###########################################################################\n\n\n')
+
+
+if a>b:
+    if a==1:
+        print(a)
+    else:
+        if a==0:
+            print(a)
+        else:
+            pass
+elif a==b:
+    print(a, b)
+else:
+    print(b)
+
+
+
+print('\n\n\n###########################################################################\n\n\n')
+
+
+
+
+c = -2
+if c<0:
+    print(-1)
+elif c==0:
+    print(0)
+elif c==1:
+    print(1)
+elif c==2:
+    print(2)
+elif c== -2:
+    print(-2)
+else:
+    print(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
