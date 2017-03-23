ef4228da807128f544f6531208b5afa1345f2e51
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2017-03-23 16:41:08 +0800

    Create 1.py

---
+++ 5-Python3/1-PythonBaoDian/Chapter-3/1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os,sys,time,math,re
+
+# int, float, complex
+
+a = 1
+b = 12.5
+print(a+b)
+
+c = 100**100
+print(c)
+print("#####")
+print(c+a)
+print("#####")
+print(c-b)
+print("##############################################################")
+
+
+print(0o11+0xa)  #按10进制输出
+print('%o' % (0o11+0xa) )  #按8进制输出
+print('%x' % (0o11+0xa) )  #按16进制输出
+print("##############################################################")
+
+
+
+m = 9.8+3j
+n = 15-2.6J
+print(m+n)
+print(n**18)
+print(n**21)
+
